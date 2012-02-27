a664e480ff35670fbdaabe31135eb9dbe77d1ee7
Gilbert(Nick) Robinson <grobins2@gmail.com>
2012-02-26 22:02:43 -0500

    Added ability to create post.

--- blog/views/views.py
+++ blog/views/views.py
@@ -1,28 +1,36 @@
 from flask import Flask, request, session, redirect, url_for,\
          abort, render_template, flash, Blueprint, escape
-
 from pymongo import Connection
+from bson import ObjectId, json_util
+from datetime import datetime
+import json
 
 connection = Connection()
-db = connection.yabs
+collection = connection['yabs'].posts
 
 main = Blueprint('main', __name__)
 
-@main.route('/posts', methods=['GET'])
-def get_posts():
- post = db.posts.find_one()
- return str(post)
+@main.route('/post/.json', methods=['GET'])
+def show_post():
+    post = [p for p in collection.find()]
+    return json.dumps(post, default=json_util.default)
 
-@main.route('/posts/new', methods=['POST'])
-def submit_post():
+@main.route('/post/new', methods=['POST'])
+def create_post():
+    author = request.form['author']
+    title  = request.form['title']
+    text   = request.form['text']
+    tags   = request.form['tags'].split(' ')
+    date   = datetime.now()
+    collection.insert({'author':author, 'title':title, 'text':text, 'tags':tags, 'date':date})
+    return True
+
+@main.route('/post/update/<int:post_id>', methods=['PUT'])
+def update_post(post_id):
     pass
 
-@main.route('/posts/update', methods=['PUT'])
-def update_post():
-    pass
-
-@main.route('/posts/delete', methods=['DELETE'])
-def delete_post():
+@main.route('/post/delete/<int:post_id>', methods=['DELETE'])
+def delete_post(post_id):
     pass
 
 
